b0981883697b431686631e0f03d4c22d0013501c
thirab <tai-lanhirabayashi@tai-lans-air.event.yale.internal>
2013-11-09 01:14:20 -0500

    please delete me
    ugh github problems

--- RedZones/src/com/example/redzones/MainActivity.java
+++ RedZones/src/com/example/redzones/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.SupportMapFragment;
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends FragmentActivity{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
