b94e30d2d22d2fc1545c4a935364ac8d0fce0411
monica <chell22m@mtholyoke.edu>
2013-11-08 23:02:33 -0500

    commit

--- RedZones/src/com/example/redzones/MainActivity.java
+++ RedZones/src/com/example/redzones/MainActivity.java
@@ -1,15 +1,22 @@
 package com.example.redzones;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
-
+public class MainActivity extends FragmentActivity {
+	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
+        SupportMapFragment fragment = new SupportMapFragment();
+        GoogleMap map = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+
     }
     
     @Override
