607a13ed656e1f195be3f9d0cba6ba5f2de19316
monica <chell22m@mtholyoke.edu>
2013-11-09 01:14:51 -0500

    map finally working!!!

--- RedZones/src/com/example/redzones/MainActivity.java
+++ RedZones/src/com/example/redzones/MainActivity.java
@@ -1,29 +1,44 @@
+//package com.example.redzones;
+//
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.SupportMapFragment;
+//
+//import android.os.Bundle;
+//import android.app.Activity;
+//import android.support.v4.app.FragmentActivity;
+//import android.view.Menu;
+//
+//public class MainActivity extends FragmentActivity {
+//	
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_main);
+////        SupportMapFragment fragment = new SupportMapFragment();
+////        GoogleMap map = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
+//        SupportMapFragment fragment = new SupportMapFragment();
+//        getSupportFragmentManager().beginTransaction().add(android.R.id.content, fragment).commit();
+//    }
+//    
+////    @Override
+////    public boolean onCreateOptionsMenu(Menu menu) {
+////        // Inflate the menu; this adds items to the action bar if it is present.
+////        getMenuInflater().inflate(R.menu.main, menu);
+////        return true;
+////    }
+//    
+//}
+
 package com.example.redzones;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.SupportMapFragment;
-
-import android.os.Bundle;
 import android.app.Activity;
-import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
+import android.os.Bundle;
 
-public class MainActivity extends FragmentActivity {
-	
+public class MainActivity extends Activity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_main);
-        SupportMapFragment fragment = new SupportMapFragment();
-        GoogleMap map = ((SupportMapFragment)  getSupportFragmentManager().findFragmentById(R.id.map)).getMap();
-
+        setContentView(R.layout.activity_main);
     }
-    
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
 }
